8db2ed72e8085ea5cf05d54972e832f798860332
TaylorCawiezell <taylorcawiezell@fullsail.edu>
2014-07-16 18:34:24 -0500

    setup for dynamic
    Made page dynamic by spliting up html format. Added user.GET to the top
    of the page.

--- simple-login/main.py
+++ simple-login/main.py
@@ -9,20 +9,29 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        page = '''<!DOCTYPE HTML>
+        page_head = '''<!DOCTYPE HTML>
 <html>
     <head>
         <title>Welcome!</title>
     </head>
-    <body>
-        <form method='GET'>
+    <body>'''
+        
+        page_body = '''<form method='GET'>
             <label>Name:</label><input type='text' name='user'/>
             <label>Name:</label><input type='text' name='email' />
-            <input type='submit' value='Submit' />
+            <input type='submit' value='Submit' />'''
+        
+        page_close = '''
         </form>
     </body>
 </html>'''
-        self.response.write(page) #printing information
+        if self.request.GET:
+            user = self.request.GET['user']
+            email = self.request.GET['email']
+            self.response.write(page_head + user + ' | ' + email + page_body + page_close)
+        else:
+            self.response.write(page_head + page_body + page_close) #printing information
+            
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
