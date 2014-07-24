c8cec4f2a9790400f522ef21aed55578e06e1edb
TaylorCawiezell <taylorcawiezell@fullsail.edu>
2014-07-24 08:23:49 -0500

    created animal class
    Created animal class to be used for inheritance.

--- what-does-the-fox-say/main.py
+++ what-does-the-fox-say/main.py
@@ -1,1 +1,26 @@
 
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+       
+        
+class Animal(object):
+    def __init__(self):
+        self._phylum = ''
+        self._class = ''
+        self._family = ''
+        self._genus = ''
+        self._url = ''
+        self._lifespan = '' + ' years'
+        self._habitat = ''
+        self._geolocation = ''
+    
+    def sound(self):
+        return self._class
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
